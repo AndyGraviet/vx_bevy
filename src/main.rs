42b65cb85cfc8863d24c7f598104262c99b5e7ff
Andy Graviet <graviet.andrew@gmail.com>
2022-08-29 15:12:59 -0600

    add button

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,9 @@ fn main() {
         .add_plugin(voxel::VoxelWorldPlugin)
         .add_plugin(debug::DebugUIPlugins)
         .add_startup_system(setup)
-        .add_startup_system(setup2)
+        .add_startup_system(addMusic)
+        .add_startup_system(addButton)
+        .add_system(button_system)
         .run();
 }
 
@@ -29,7 +31,69 @@ fn setup(mut cmds: Commands) {
     .insert(bevy_atmosphere::plugin::AtmosphereCamera(None));
 }
 
-fn setup2(asset_server: Res<AssetServer>, audio: Res<Audio>) {
+fn addMusic(asset_server: Res<AssetServer>, audio: Res<Audio>) {
     let music = asset_server.load("../assets/sounds/sound1.ogg");
     audio.play(music);
+}
+
+
+const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
+const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
+const PRESSED_BUTTON: Color = Color::rgb(0.35, 0.75, 0.35);
+
+fn button_system(
+    mut interaction_query: Query<
+    (&Interaction, &mut UiColor, &Children),
+    (Changed<Interaction>, With<Button>),
+>,
+mut text_query: Query<&mut Text>,
+) {
+    for (interaction, mut color, children) in &mut interaction_query {
+        let mut text = text_query.get_mut(children[0]).unwrap();
+        match *interaction {
+            Interaction::Clicked => {
+                text.sections[0].value = "Press".to_string();
+                *color = PRESSED_BUTTON.into();
+            }
+            Interaction::Hovered => {
+                text.sections[0].value = "Hover".to_string();
+                *color = HOVERED_BUTTON.into();
+            }
+            Interaction::None => {
+                text.sections[0].value = "Button".to_string();
+                *color = NORMAL_BUTTON.into();
+            }
+        }
+    }
+}
+
+
+fn addButton(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // ui camera
+    commands.spawn_bundle(Camera2dBundle::default());
+    commands
+        .spawn_bundle(ButtonBundle {
+            style: Style {
+                size: Size::new(Val::Px(150.0), Val::Px(65.0)),
+                // center button
+                margin: UiRect::all(Val::Auto),
+                // horizontally center child text
+                justify_content: JustifyContent::Center,
+                // vertically center child text
+                align_items: AlignItems::Center,
+                ..default()
+            },
+            color: NORMAL_BUTTON.into(),
+            ..default()
+        })
+        .with_children(|parent| {
+            parent.spawn_bundle(TextBundle::from_section(
+                "Button",
+                TextStyle {
+                    font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                    font_size: 40.0,
+                    color: Color::rgb(0.9, 0.9, 0.9),
+                },
+            ));
+        });
 }
